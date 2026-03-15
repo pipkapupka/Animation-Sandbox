c144fecdbb55c8ec9378113a912e83abbe2afd3b
Pipka Pupka <pipkapupka275@gmail.com>
2026-03-15 13:59:11 +0500

    feat: add protected default contructor for SandboxApp

--- include/IApplication.h
+++ include/IApplication.h
@@ -4,10 +4,13 @@ class IApplication {
 public:
     virtual ~IApplication() = default;
     virtual void Initialize() = 0;
+    virtual void Run() = 0;
     virtual void Update(float delta_time) = 0;
     virtual void Render(float aspect_ratio) = 0;
     virtual void Shutdown() = 0;
 private:
     IApplication(const IApplication&) = delete;
     IApplication& operator=(const IApplication&) = delete;
+protected:
+    IApplication() = default;
 };
